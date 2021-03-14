6f9e564c6470ce2e7e1e2c7220e63e62e35446f6
sprintqaDevOps <devops@sprintqa.com>
2021-03-14 11:17:45 -0700

    added more variables

--- variables.tf
+++ variables.tf
@@ -62,6 +62,12 @@ variable prefix {
 
 variable ami {
     type = string
+    description = "The id of the machine image (AMI) to use for the server.”
+    validation {
+                condition             = length(var.ami) > 4 && substr(var.ami, 0, 4) == "ami-"
+                error_message         = "The ami value must be a valid AMI id, starting with \"ami-\"." 
+        }
+
 }
 
 variable instance_type {
