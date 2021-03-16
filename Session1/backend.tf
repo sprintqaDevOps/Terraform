471486c264c98dbfac15022376de8cbc88f902b0
zsalieva <zuhrasalieva@gmail.com>
2021-03-16 12:02:52 -0500

    files updated

--- Session1/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-files-mcalik"
-    key    = "tfstate/webserver.tfstate"
-    region = "us-east-1"
-  }
-}
--- Session2/backend.tf
+++ Session2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-files-mcalik"
-    key    = "tfstate/data-source.tfstate"
+    bucket = "terraform-state-files-zsalieva"
+    key    = "tf-state/data-source.tfstate"
     region = "us-east-1"
   }
 }
