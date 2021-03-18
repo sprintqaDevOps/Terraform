22ba1e04d6edfc5f2e39ed405155d88360c835d7
sprintqaDevOps <devops@sprintqa.com>
2021-03-17 17:46:15 -0700

    added user data to install docker

--- Session3/variables.tf
+++ Session3/variables.tf
@@ -31,10 +31,11 @@ variable "prefix" {
 }
 
 variable "ami" {
-  type = map(any)
+  type = map(string)
   default = {
     "us-east-1" = "ami-038f1ca1bd58a5790"
     "us-east-2" = "ami-07a0844029df33d7d"
+    "us-west-1" = "ami-0c7945b4c95c0481c"
   }
 }
 variable "instance_type" {
