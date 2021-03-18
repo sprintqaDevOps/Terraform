7fe5652c8151065d3df1a43a0a382f646f52512e
sprintqaDevOps <devops@sprintqa.com>
2021-03-17 19:19:50 -0700

    added count and created multiple ec2

--- Session3/outputs.tf
+++ Session3/outputs.tf
@@ -1,8 +1,8 @@
 output "instance_ip_addr" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.web[*].public_ip
 }
 output "arn" {
-  value = aws_instance.web.arn
+  value = aws_instance.web[*].arn
 }
 
 
