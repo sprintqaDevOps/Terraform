85f2895c1e6994d7c09add42335bb1ca5b08007d
sprintqaDevOps <devops@sprintqa.com>
2021-03-28 12:13:58 -0700

    condition

--- Session10/loops.tf
+++ Session10/loops.tf
@@ -7,18 +7,18 @@
 #   value = [for s in local.list : upper(s)]
 # }
 
-# list to map
-locals {
-  names = ["a","b","c"]
-}
-output "result" {
-  value = {for index, name in local.names: index => name }
-}
+# # list to map
+# locals {
+#   names = ["a","b","c"]
+# }
+# output "result" {
+#   value = {for index, name in local.names: index => name }
+# }
 
 # # filtering
 # locals {
 #   list = [1,2,3,4,5]
 # }
 # output "list" {
-#   value = [for i in local.list : i if i < 3]
+#   value = [for i in local.list : i if i > 3]
 # }
--- Session10/subnets.tf
+++ Session10/subnets.tf
@@ -1,51 +1,52 @@
-provider "aws" {
-   region = "us-east-1"
-}
-resource "aws_vpc" "my_vpc" {
+# provider "aws" {
+#    region = "us-east-1"
+# }
+# resource "aws_vpc" "my_vpc" {
 
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
 
-  tags = {
-    Name = "my_vpc"
-  }
+#   tags = {
+#     Name = "my_vpc"
+#   }
 
-}
+# }
 
-# # 
-variable "cidr_block_public" {
 
-  default = ["10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
+# # # 
+# variable "cidr_block_public" {
+
+#   default = ["10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
   
-}
+# }
 
-variable "cidr_block_private" {
+# variable "cidr_block_private" {
 
-  default = ["10.0.9.0/24", "10.0.10.0/24"]
+#   default = ["10.0.9.0/24", "10.0.10.0/24"]
   
-}
+# }
 
-# # 3 public subnet with this cidr_block_public using for_each 
-# # 2 private subnet with this cidr_block_private using for_each 
+# # # 3 public subnet with this cidr_block_public using for_each 
+# # # 2 private subnet with this cidr_block_private using for_each 
 
-resource "aws_subnet" "public_subnet" {
-#   for_each = toset(var.cidr_block_public)
-  for_each = {for index, cidr in var.cidr_block_public : index => cidr}
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = each.value
-  map_public_ip_on_launch = true
-}
+# resource "aws_subnet" "public_subnet" {
+# #   for_each = toset(var.cidr_block_public)
+#   for_each = {for index, cidr in var.cidr_block_public : index => cidr}
+#   vpc_id = aws_vpc.my_vpc.id
+#   cidr_block = each.value
+#   map_public_ip_on_launch = true
+# }
 
-resource "aws_subnet" "private_subnet" {
-  # for_each = toset(var.cidr_block_private)
-  for_each = {for index, cidr in var.cidr_block_private : index => cidr}
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = each.value
-  map_public_ip_on_launch = false
-}
+# resource "aws_subnet" "private_subnet" {
+#   # for_each = toset(var.cidr_block_private)
+#   for_each = {for index, cidr in var.cidr_block_private : index => cidr}
+#   vpc_id = aws_vpc.my_vpc.id
+#   cidr_block = each.value
+#   map_public_ip_on_launch = false
+# }
 
-output "cidr_private" {
+# output "cidr_private" {
 
-  value = {for index, cidr in var.cidr_block_private : index => cidr}
+#   value = {for index, cidr in var.cidr_block_private : index => cidr}
   
-}
+# }
