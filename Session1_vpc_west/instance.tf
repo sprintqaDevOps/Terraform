04b72e4676ff84721c613624c9637372f83e558a
zsalieva <zuhrasalieva@gmail.com>
2021-05-17 15:41:15 -0500

    crreating VPC in us-West-2

---
+++ Session1_vpc_west/instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "web" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg_1.id]
+  subnet_id              = aws_subnet.public_subnet_1.id
+  key_name               = var.key_name
+
+  tags = {
+    Name = "${var.prefix} Second_ec2"
+  }
+}
